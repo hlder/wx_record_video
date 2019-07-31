48a8f2793d687a7a53ffb1fae32de60297cfe934
hld <sald@hlddeMacBook-Pro.local>
2019-07-31 18:41:56 +0800

    添加library

--- app/src/main/java/com/hld/recordvideo/MainActivity.java
+++ app/src/main/java/com/hld/recordvideo/MainActivity.java
@@ -20,6 +20,7 @@ public class MainActivity extends AppCompatActivity {
             public void onClick(View v) {
                 Intent intent=new Intent(MainActivity.this,RecordVideoActivity.class);
                 startActivityForResult(intent,1);
+
             }
         });
 
