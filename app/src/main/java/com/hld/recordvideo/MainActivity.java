d5cbb55f8c4665bd592fa9950c6fb4a96ca8ce02
hld <sald@hlddeMacBook-Pro.local>
2019-08-01 09:15:56 +0800

    demo

--- app/src/main/java/com/hld/recordvideo/MainActivity.java
+++ app/src/main/java/com/hld/recordvideo/MainActivity.java
@@ -5,25 +5,41 @@ import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
+import android.widget.TextView;
 
 import com.hld.recordlibrary.RecordVideoActivity;
 
 public class MainActivity extends AppCompatActivity {
 
+    private final int REQUEST_RECORDVIDEO=1;
+
+    private TextView textView;
+
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        textView=findViewById(R.id.textView);
+
 
         findViewById(R.id.button).setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 Intent intent=new Intent(MainActivity.this,RecordVideoActivity.class);
-                startActivityForResult(intent,1);
+                startActivityForResult(intent,REQUEST_RECORDVIDEO);
 
             }
         });
 
+    }
 
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if(requestCode==REQUEST_RECORDVIDEO&&resultCode==RESULT_OK){
+            String filePath=data.getStringExtra("path");
+            textView.setText("视频位置:"+filePath);
+
+        }
     }
 }
